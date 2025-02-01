da1bfd91ee9dfb39fb27efe8b9aeebd0e38d46b3
leofilllium <134720227+dilnurayu@users.noreply.github.com>
2025-02-01 11:58:48 +0500

    deleted GuideBook container

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,59 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Footer from './skeleton/Footer';
-import Header from './skeleton/Header';
-import Content from './landing/Content';
-import LandingBlock from './landing/LandingBlock';
-import GuidesList from './guide/GuidesList'; 
-import SearchSection from './guide/SearchSection';
-import StartJourney from './guide/StartJourney';
-import ToursList from './tour/ToursList';
-import GuideDetails from './guide/GuideDetails';
-import ScrollToTop from './ScrollToTop';
-import GuideBook from './guide/GuideBook';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./skeleton/Footer";
+import Header from "./skeleton/Header";
+import Content from "./landing/Content";
+import LandingBlock from "./landing/LandingBlock";
+import GuidesList from "./guide/GuidesList";
+import SearchSection from "./guide/SearchSection";
+import StartJourney from "./guide/StartJourney";
+import ToursList from "./tour/ToursList";
+import GuideDetails from "./guide/GuideDetails";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <Router>
-      <ScrollToTop/>
+      <ScrollToTop />
       <Header />
       <Routes>
-        <Route path="/" element={
-          <>
-            <LandingBlock />
-            <Content />
-          </>
-        } />
-        <Route path="/guides" element={
-          <>
-            <SearchSection />
-            <GuidesList />
-            <StartJourney />
-          </>
-        } />
-        <Route path="/guides/:id" element={
-          <>
-            <GuideDetails />
-            <GuideBook />
-          </>
-        } />
-        <Route path="/destinations" element={
-          <>
-            <SearchSection />
-            <ToursList />
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <LandingBlock />
+              <Content />
+            </>
+          }
+        />
+        <Route
+          path="/guides"
+          element={
+            <>
+              <SearchSection />
+              <GuidesList />
+              <StartJourney />
+            </>
+          }
+        />
+        <Route
+          path="/guides/:id"
+          element={
+            <>
+              <GuideDetails />
+            </>
+          }
+        />
+        <Route
+          path="/destinations"
+          element={
+            <>
+              <SearchSection />
+              <ToursList />
+            </>
+          }
+        />
       </Routes>
       <Footer />
     </Router>
