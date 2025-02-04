213351a7f5b37725d03f28d37bb756bb81a4067a
leofilllium <134720227+dilnurayu@users.noreply.github.com>
2025-02-04 11:38:20 +0500

    some fake tours duplication

--- src/guide-tour/GuideToursList.jsx
+++ src/guide-tour/GuideToursList.jsx
@@ -32,6 +32,29 @@ const GuideToursList = () => {
           </div>
         </div>
 
+        <div className="guide-tour-list-container">
+          <div className="guide-tour-list-left">
+            <h3>Tashkent - Samarkand - Bukhara - Khiva: Pearl of the East</h3>
+            <p>4 Days 3 Nights | English, Spanish</p>
+            <p>Price: $250.00/per person</p>
+          </div>
+          <div className="tours-buttons">
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        </div>
+        <div className="guide-tour-list-container">
+          <div className="guide-tour-list-left">
+            <h3>Tashkent - Samarkand - Bukhara - Khiva: Pearl of the East</h3>
+            <p>4 Days 3 Nights | English, Spanish</p>
+            <p>Price: $250.00/per person</p>
+          </div>
+          <div className="tours-buttons">
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        </div>
+
         {isModalOpen && (
           <div className="modal-overlay" onClick={closeModal}>
             <div
